27255222b46afef982f35a0a17372b86d451ec68
IGieckI <giacomo.antonelli3@gmail.com>
2026-03-05 20:19:14 +0100

    [PORTING] Change of pin values (FAKE VALUES TO MAKE THE BUILD POSSIBLE)

--- components/global/src/pins.h
+++ components/global/src/pins.h
@@ -23,23 +23,23 @@
 #define I2C_MASTER_RX_BUF_DISABLE 0
 
 // SD Card pins.
-#define SD_CLK D13
-#define SD_SO D12
-#define SD_SI D11
-#define SD_CS D10
+#define SD_CLK 21// from Arduino D13
+#define SD_SO 38// from Arduino D12
+#define SD_SI 47// from Arduino D11
+#define SD_CS 48// from Arduino D10
 #define SD_DET -1
 
 // Actuators
-#define DROGUE_ACTUATOR_PIN D0
-#define MAIN_ACTUATOR_PIN D1
-#define BUZZER_PIN D2
+#define DROGUE_ACTUATOR_PIN 5 // Replaced D0 with actual pin number 5
+#define MAIN_ACTUATOR_PIN 4  // Replaced D1 with actual pin number 4
+#define BUZZER_PIN 6         // Replaced D2 with actual pin number 6
 
 // LED
 #define LED_RED_PIN A2
 #define LED_GREEN_PIN A3
 #define LED_BLUE_PIN A1
 
-#define GPS_LED D6
+#define GPS_LED 7            // Replaced D6 with actual pin number 7
 
-#define ARMING_PIN D6
+#define ARMING_PIN 7         // Replaced D6 with actual pin number 7
 
