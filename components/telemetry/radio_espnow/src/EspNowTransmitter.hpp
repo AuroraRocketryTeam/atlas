6bb9676f3258e63d4b507401fbb7ca591dddc478
IGieckI <giacomo.antonelli3@gmail.com>
2026-03-17 23:33:14 +0100

    [BUGFIX] Update onDataSent callback signature to use uint8_t* for MAC address

--- components/telemetry/radio_espnow/src/EspNowTransmitter.hpp
+++ components/telemetry/radio_espnow/src/EspNowTransmitter.hpp
@@ -84,7 +84,7 @@ private:
     
     // ESP-NOW callbacks (must be static)
     // Note: Signature matching esp_now_send_cb_t
-    static void onDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status);
+    static void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
     
     // Instance pointer for callback access
     static EspNowTransmitter* instance;
