3fb5dc8b1af5041dee9206ea49d9a2fe339ebd45
IGieckI <giacomo.antonelli3@gmail.com>
2026-04-13 22:50:26 +0200

    [REFACTOR] Remove redundant logging of sensor data in RocketModel updates

--- components/model/RocketModel.cpp
+++ components/model/RocketModel.cpp
@@ -54,7 +54,6 @@ bool RocketModel::updateBNO055() {
     bool result = _bno->updateData();
 
     _bnoData = _bno->getData();
-    _logger->logSensorData(_bnoData);
 
     return result;
 }
@@ -63,7 +62,6 @@ bool RocketModel::updateLIS3DHTR() {
     bool result = _lis3dh->updateData();
 
     _lis3dhData = _lis3dh->getData();
-    _logger->logSensorData(_lis3dhData);
 
     return result;
 }
@@ -72,7 +70,6 @@ bool RocketModel::updateMS561101BA03_1() {
     bool result = _ms56_1->updateData();
 
     _ms561101ba03Data_1 = _ms56_1->getData();
-    _logger->logSensorData(_ms561101ba03Data_1);
 
     return result;
 }
@@ -81,7 +78,6 @@ bool RocketModel::updateMS561101BA03_2() {
     bool result = _ms56_2->updateData();
 
     _ms561101ba03Data_2 = _ms56_2->getData();
-    _logger->logSensorData(_ms561101ba03Data_2);
 
     return result;
 }
@@ -90,7 +86,6 @@ bool RocketModel::updateGPS() {
     bool result = _gps->updateData();
 
     _gpsData = _gps->getData();
-    _logger->logSensorData(_gpsData);
 
     return result;
 }
