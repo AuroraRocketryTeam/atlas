a06e8816b3a11e393768e84e46ec62b6f3a5c8bb
Federico Bosi <federico@rhxto.com>
2026-06-17 16:48:36 +0200

    Default configuration implementation

--- components/telemetry/radio_lora/src/E220LoRaTransmitter.cpp
+++ components/telemetry/radio_lora/src/E220LoRaTransmitter.cpp
@@ -38,6 +38,25 @@ ResponseStatusContainer E220LoRaTransmitter::init(Configuration config)
     return result;
 }
 
+Configuration E220LoRaTransmitter::defaultConfiguration()
+{
+    Configuration config = {};
+    config.ADDH = LORA_ADDH;
+    config.ADDL = LORA_ADDL;
+    config.CHAN = LORA_CHANNEL;
+    config.SPED.uartBaudRate  = UART_BPS_115200;
+    config.SPED.airDataRate   = AIR_DATA_RATE_100_96;
+    config.SPED.uartParity    = MODE_00_8N1;
+    config.OPTION.subPacketSetting  = SPS_200_00;
+    config.OPTION.RSSIAmbientNoise  = RSSI_AMBIENT_NOISE_DISABLED;
+    config.OPTION.transmissionPower = POWER_17;
+    config.TRANSMISSION_MODE.enableRSSI        = RSSI_ENABLED;
+    config.TRANSMISSION_MODE.fixedTransmission = FT_FIXED_TRANSMISSION;
+    config.TRANSMISSION_MODE.enableLBT         = LBT_DISABLED;
+    config.TRANSMISSION_MODE.WORPeriod         = WOR_2000_011;
+    return config;
+}
+
 /**
  * @brief Converts the data to a byte array.
  *
